9713607b9d98eab92b4ab980988e751a64ce5c03
Karl Belousov <belousovkarl@gmail.com>
2023-11-07 20:16:38 +0600

    адаптив и стилизация футера

--- source/js/app.js
+++ source/js/app.js
@@ -1,12 +1,12 @@
-let pageHeaderNav = document.querySelector('.page-header__nav');
-let pageHeaderToggler = document.querySelector('.page-header__toggler');
+let pageHeaderNav = document.querySelector(".page-header__nav");
+let pageHeaderToggler = document.querySelector(".page-header__toggler");
 
-pageHeaderToggler.addEventListener('click', function(){
-  if (pageHeaderNav.classList.contains('page-header__nav--closed')) {
-    pageHeaderNav.classList.remove('page-header__nav--closed');
-    pageHeaderNav.classList.add('page-header__nav--opened');
+pageHeaderToggler.addEventListener("click", function () {
+  if (pageHeaderNav.classList.contains("page-header__nav--closed")) {
+    pageHeaderNav.classList.remove("page-header__nav--closed");
+    pageHeaderNav.classList.add("page-header__nav--opened");
   } else {
-    pageHeaderNav.classList.add('page-header__nav--closed');
-    pageHeaderNav.classList.remove('page-header__nav--opened');
+    pageHeaderNav.classList.add("page-header__nav--closed");
+    pageHeaderNav.classList.remove("page-header__nav--opened");
   }
 });
